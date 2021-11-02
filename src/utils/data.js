b8e9c6e883277b7011077c9bfbab519bd0f9cf08
Tomivan <gracejunkie20@gmail.com>
2021-11-02 10:06:33 +0100

    committed for storing

---
+++ src/utils/data.js
@@ -0,0 +1,81 @@
+export const mockData = [
+    {
+        adminData:[ 
+            {
+            "name": "Oluwatosin Omoboyejo",
+            "email": "oluwatosinracheal@yahoo.com",
+            "phoneNumber": "2514262819",
+            "role": " Super Admin"
+        },
+        {
+            "name": "Falade Damilola",
+            "email": "iyanufalay@gmail.com",
+            "phoneNumber": "2514262819",
+            "role": "Admin"
+        },
+    ]
+    },
+    {
+        caretakerData: [
+            {
+                "firstname": "Toluwalase",
+                "lastname": "Idowu",
+                "email": "idtunmise@gmail.com",
+                "phoneNumber": "9023842840"
+            },
+            {
+                "firstname": "Helen",
+                "lastname": "Adefemi",
+                "email": "prettyhelen@gmail.com",
+                "phoneNumber": "9374727920",
+            }
+        ]
+    },
+    {
+        healthData: [
+            {
+                "firstname": "Esther",
+                "lastName": "Oluwayomi",
+                "email": "estheroluwayomi@gmail.com",
+                "title": "Doctor",
+                "address": "20, Omorinre Johnson, Lekki, Lagos",
+                "degree": "Ph.D",
+                "licenseNumber": "2737290-8377328",
+                "phoneNumber": "9372637203",
+                "medicalField": "Psychiatry",
+                "status": "active",
+                "bio" : "A fun loving Ekiti queen"
+            },
+            {
+                "firstname": "Damilola",
+                "lastName": "adegbenro",
+                "email": "adegbenromercy@gmail.com",
+                "title": "Doctor",
+                "address": "5, Markudi Street, Akure, Ondo",
+                "degree": "Ph.D",
+                "licenseNumber": "2737290-8377328",
+                "phoneNumber": "9928320910",
+                "medicalField": "Neurosurgery",
+                "status": "suspended",
+                "bio": "A strict disciplinarian with a warm heart and a pssion for God"
+            },
+        ]
+    }, 
+    {
+        medicalData: [
+            {
+                "healthWorker": "Adegbenro Damilola",
+                "patient": "Boluwatife Adebisi",
+                "note": "Diagnosed with stage 2 brain cancer"
+            },
+            {
+                "healthWorker": "Esther Oluwayomi",
+                "patient": "Christiana adeoye",
+                "note": "Diagnosed with celebral malaria"
+            },
+        ]
+    },
+    {
+        patientData: []
+    }
+]
